b1bfcde5432cc999f498e5ece31fe5dde5dd66fc
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-06-13 11:31:37 +0300

    Assignment Done

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const add = (param1, param2) => {
+    return param1 + param2;
+};
+
+
+console.log(add(3,4))
+
+const divide = (a, b) => {
+    return 2000 / 100;
+}
+console.log(divide(2000, 100));
+
+const square = (z) => {
+    return z * z;
+}
+console.log(square(z));
